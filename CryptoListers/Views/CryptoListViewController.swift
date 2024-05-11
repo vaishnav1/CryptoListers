18724b5461fac300d82006688ff586b400408f05
vaishnav1 <vaishnav.kukku52@gmail.com>
2024-05-11 15:19:41 +0530

    Initialising project with first commit and removing Storyboard references

---
+++ CryptoListers/Views/CryptoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoListViewController.swift
+//  CryptoListers
+//
+//  Created by Vaishnav on 11/05/24.
+//
+
+import UIKit
+
+class CryptoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
